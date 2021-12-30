61c86aa73ea4eb86730708159ae9899e0378fef4
EfaSer <62093140+EfaSer@users.noreply.github.com>
2021-12-30 16:22:37 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,154 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        //dynamicBullets: true,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".product-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        //dynamicBullets: true,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+          },
+        640: {
+          slidesPerView: 2,
+          
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+  });
+
+  var swiper = new Swiper(".revies-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+          },
+        640: {
+          slidesPerView: 2,
+          
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        
+      },
+  });
+
+  var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+          },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+          },
+      },
+    
+  });
+
+  var swiper = new Swiper(".clients-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        //dynamicBullets: true,
+      },
+    /*navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },*/
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+          },
+        640: {
+          slidesPerView: 2,
+          
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+  });
